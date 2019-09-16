2dadc0617acbc8a476205e5a0e41135370067230
Khang Hoang <hoangtrieukhang@gmail.com>
2019-09-15 19:45:29 -0700

    refactor code

--- packages/puppet-timeline-gif/examples/index.js
+++ packages/puppet-timeline-gif/examples/index.js
@@ -1,9 +1,9 @@
-const { getPage, closePage } = require('puppeteer-pool');
+const { getPage, closePage } = require('puppet-pool');
 const { startCapture, endCapture } = require('../');
 
 (async () => {
     const page = await getPage();
-    const { endCapture } =await startCapture(page);
+    const { endCapture } = await startCapture(page);
 
     await page.goto('https://www.apple.com');
     await page.goto('https://www.google.com');
--- packages/puppet-timeline-gif/index.js
+++ packages/puppet-timeline-gif/index.js
@@ -1,24 +1,22 @@
-const { getPage, closePage } = require('puppeteer-pool');
+const { getPage, closePage } = require('puppet-pool');
 const os = require('os');
-const { readJson, outputJson } = require('fs-extra');
+const { readJson } = require('fs-extra');
 const { resolve } = require('path');
 const snapline = require('snapline');
+const uuidv1 = require('uuid/v1');
 
 module.exports = {
-
     startCapture: async (page) => {
         const client = await page.target().createCDPSession();
         await client.send('Performance.enable');
 
         const tempPath = os.tmpdir();
-        const filePath = resolve(tempPath, 'trace.json');
+        const filePath = resolve(tempPath, `${uuidv1()}.json`);
         await page.tracing.start({ path: filePath, screenshots: true });
         return {
             endCapture: async () => {
                 await page.tracing.stop();
                 const contentFile = await readJson(filePath);
-                await outputJson(filePath, contentFile.traceEvents);
-
                 const gifPath = await snapline.toGif(contentFile.traceEvents);
                 console.log(`The gif is ready: ${gifPath}!`);
             }
